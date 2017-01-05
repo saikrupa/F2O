9d7e6338773a2583d772a771f9d67ea1777a3233
saikrupa <puram.saikrupa@gmail.com>
2017-01-05 14:23:15 +0530

    5-1-2017 commit with sample database

--- app/src/main/java/com/f2o/om/virinchi/f2o/App/NetworkStateReceiver.java
+++ app/src/main/java/com/f2o/om/virinchi/f2o/App/NetworkStateReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Ndroid on 1/5/2017.
@@ -13,15 +14,20 @@ import android.util.Log;
 
 public class NetworkStateReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
-        Log.d("app","Network connectivity change");
-        if(intent.getExtras()!=null) {
-            NetworkInfo ni=(NetworkInfo) intent.getExtras().get(ConnectivityManager.EXTRA_NETWORK_INFO);
-            if(ni!=null && ni.getState()==NetworkInfo.State.CONNECTED) {
-                Log.i("app","Network "+ni.getTypeName()+" connected");
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (activeNetwork != null) { // connected to the internet
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                // connected to wifi
+                Toast.makeText(context, activeNetwork.getTypeName(), Toast.LENGTH_SHORT).show();
+            } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                // connected to the mobile provider's data plan
+                Toast.makeText(context, activeNetwork.getTypeName(), Toast.LENGTH_SHORT).show();
             }
-        }
-        if(intent.getExtras().getBoolean(ConnectivityManager.EXTRA_NO_CONNECTIVITY,Boolean.FALSE)) {
-            Log.d("app","There's no network connectivity");
+        } else {
+            // not connected to the internet
+            Toast.makeText(context, "not Connected", Toast.LENGTH_SHORT).show();
+
         }
     }
 }
